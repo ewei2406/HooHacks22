88659e11b55134f30890ef27f70b32a5b3631651
Ravi Jayaraman <spy7sw@virginia.edu>
2022-03-26 15:59:13 -0400

    yes

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -2,10 +2,22 @@ import React from "react"
 import styled from "styled-components"
 
 const TitleWrapper = styled.div`
+    font-size:45px;
+    text-align: center;
+    line-height: 80px;
+`
+
+const TitleBorder = styled.div`
+    width: 100%;
+    height: 80px;
+    background-color: ${p => p.theme.accentColor};
+
 `
 
 const Title = ({ text }) => {
-    return ( <TitleWrapper>{text}</TitleWrapper>)
+    return ( <TitleBorder>
+                <TitleWrapper>{text}</TitleWrapper>
+            </TitleBorder> )
 }
 
 export default Title
